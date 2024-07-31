5ac367e4b7e2560e21f30ae1d9066f45df56f7c0
Ondřej Častoral <ondrejcastoral@email.cz>
2024-07-31 19:52:58 +0200

    initial commit

---
+++ src/main/java/org/castixz/address_loader/AddressLoaderApplication.java
@@ -0,0 +1,13 @@
+package org.castixz.address_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/castixz/address_loader/AddressLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.castixz.address_loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
