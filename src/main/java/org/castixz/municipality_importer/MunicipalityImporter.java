fb13e0248c8a80722bfbd2f58e3be4c6994aa932
Ondřej Častoral <ondrejcastoral@email.cz>
2024-08-02 00:53:27 +0200

    add cleaning of db

--- src/main/java/org/castixz/municipality_importer/MunicipalityImporter.java
+++ src/main/java/org/castixz/municipality_importer/MunicipalityImporter.java
@@ -2,8 +2,12 @@ package org.castixz.municipality_importer;
 
 import org.castixz.municipality_importer.batch.BatchService;
 import org.castixz.municipality_importer.config.AppConfig;
+import org.castixz.municipality_importer.repository.MunicipalityPartRepository;
+import org.castixz.municipality_importer.repository.MunicipalityRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MunicipalityImporter {
@@ -14,6 +18,14 @@ public class MunicipalityImporter {
 		var batchService = ctx.getBean(BatchService.class);
 		batchService.runJobByJobType(appConfig.getTaskType());
 		System.exit(0);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(MunicipalityPartRepository municipalityPartRepository, MunicipalityRepository municipalityRepository) {
+		return args -> {
+			municipalityPartRepository.deleteAll();
+			municipalityRepository.deleteAll();
+		};
 	}
 
 }
