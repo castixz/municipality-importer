efc943015c6d646ec68b39bd6256748e2df8d01a
Ondřej Častoral <ondrejcastoral@email.cz>
2024-08-02 00:20:07 +0200

    implement main class

--- src/main/java/org/castixz/municipality_importer/MunicipalityImporter.java
+++ src/main/java/org/castixz/municipality_importer/MunicipalityImporter.java
@@ -1,5 +1,7 @@
 package org.castixz.municipality_importer;
 
+import org.castixz.municipality_importer.batch.BatchService;
+import org.castixz.municipality_importer.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MunicipalityImporter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MunicipalityImporter.class, args);
+		var ctx = SpringApplication.run(MunicipalityImporter.class, args);
+		var appConfig = ctx.getBean(AppConfig.class);
+		var batchService = ctx.getBean(BatchService.class);
+		batchService.runJobByJobType(appConfig.getTaskType());
+		System.exit(0);
 	}
 
 }
