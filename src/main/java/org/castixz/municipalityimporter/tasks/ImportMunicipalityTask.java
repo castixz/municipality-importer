7441548e56f733a6b1255cd248078a8b888358ba
Ondřej Častoral <ondrejcastoral@email.cz>
2024-08-02 10:09:40 +0200

    refactor ImportMunicipalityTask

--- src/main/java/org/castixz/municipalityimporter/tasks/ImportMunicipalityTask.java
+++ src/main/java/org/castixz/municipalityimporter/tasks/ImportMunicipalityTask.java
@@ -1,22 +1,22 @@
 package org.castixz.municipalityimporter.tasks;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.castixz.municipalityimporter.enums.TaskResult;
 import org.castixz.municipalityimporter.exceptions.FileDownloadFailedException;
 import org.castixz.municipalityimporter.file.FileUtils;
 import org.castixz.municipalityimporter.mapper.MunicipalityMapper;
 import org.castixz.municipalityimporter.mapper.MunicipalityPartMapper;
+import org.castixz.municipalityimporter.parser.MunicipalityParsingResult;
 import org.castixz.municipalityimporter.parser.MunicipalityXMLParser;
 import org.castixz.municipalityimporter.repository.MunicipalityPartRepository;
 import org.castixz.municipalityimporter.repository.MunicipalityRepository;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 
@@ -25,22 +25,19 @@ import java.nio.file.Path;
 @Slf4j
 public class ImportMunicipalityTask implements BatchTask {
 
+    private static final String ZIP_FILE_NAME = "municipality.zip";
+
+    private static final String XML_FILE_NAME = "20210331_OB_573060_UZSZ.xml";
+
     private static final URL URL_TO_MUNICIPALITY_FILE;
 
-    private static final Path WORKDIR_PATH;
-
-    private static final Path WORKDIR_MUNICIPALITY_ZIP;
-
-    private static final Path FILE_TO_PARSE_PATH;
+    private static final Path WORKDIR_PATH = Path.of(System.getProperty("user.dir") + "/workdir");
 
     static {
         try {
-            URL_TO_MUNICIPALITY_FILE = new URI("""
+            URL_TO_MUNICIPALITY_FILE = URI.create("""
                     https://www.smartform.cz/download/kopidlno.xml.zip""").toURL();
-            WORKDIR_PATH = Path.of(System.getProperty("user.dir") + "/workdir");
-            WORKDIR_MUNICIPALITY_ZIP = Path.of(WORKDIR_PATH + "/municipality.zip");
-            FILE_TO_PARSE_PATH = Path.of(WORKDIR_PATH + "/20210331_OB_573060_UZSZ.xml");
-        } catch (MalformedURLException | URISyntaxException e) {
+        } catch (MalformedURLException e) {
             throw new FileDownloadFailedException("URI is not valid", e);
         }
     }
@@ -56,24 +53,33 @@ public class ImportMunicipalityTask implements BatchTask {
     private final MunicipalityPartRepository municipalityPartRepository;
 
     @Override
-    public void execute() {
-        log.info("File fetching starting");
-        FileUtils.getFileByUrlAndSave(URL_TO_MUNICIPALITY_FILE, WORKDIR_MUNICIPALITY_ZIP);
-        log.info("File fetching finished");
-        FileUtils.unzip(WORKDIR_MUNICIPALITY_ZIP.toString(), WORKDIR_PATH.toString());
+    public TaskResult execute() {
         try {
-            Files.delete(WORKDIR_MUNICIPALITY_ZIP);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            this.preprocessTheFile();
+            var result = municipalityXMLParser.parse(Path.of(WORKDIR_PATH.toString(), XML_FILE_NAME));
+            this.saveToDB(result);
+            return TaskResult.SUCCESSFULL;
+        } catch (Exception e) {
+            log.error("Task execution failed", e);
+            return TaskResult.FAIL;
         }
-        var result = municipalityXMLParser.parse(FILE_TO_PARSE_PATH);
-        log.info("Starting inserting municipalities to DB");
-        result.municipalityDTOList()
-                .stream()
-                .map(municipalityMapper::toDAO)
-                .forEach(municipalityRepository::save);
-        log.info("Inserting municipalities to DB has been finished");
+    }
 
+    @SneakyThrows
+    void preprocessTheFile() {
+        log.info("File fetching starting");
+        var targetPathForMunicipalityZipFile = Path.of(WORKDIR_PATH.toString(), ZIP_FILE_NAME);
+        FileUtils.getFileByUrlAndSave(URL_TO_MUNICIPALITY_FILE, targetPathForMunicipalityZipFile);
+        log.info("File fetching finished");
+        FileUtils.unzip(targetPathForMunicipalityZipFile.toString(), WORKDIR_PATH.toString());
+    }
+
+    void saveToDB(MunicipalityParsingResult result) {
+        this.saveMunicipalitiesToDB(result);
+        this.saveMunicipalityPartsToDB(result);
+    }
+
+    void saveMunicipalityPartsToDB(MunicipalityParsingResult result) {
         log.info("Starting inserting municipality parts to DB");
         result.municipalityPartDTOList().stream()
                 .map(municipalityPartDTO -> {
@@ -84,5 +90,14 @@ public class ImportMunicipalityTask implements BatchTask {
                 })
                 .forEach(municipalityPartRepository::save);
         log.info("Inserting municipality parts to DB has been finished");
+    }
+
+    void saveMunicipalitiesToDB(MunicipalityParsingResult result) {
+        log.info("Starting inserting municipalities to DB");
+        result.municipalityDTOList()
+                .stream()
+                .map(municipalityMapper::toDAO)
+                .forEach(municipalityRepository::save);
+        log.info("Inserting municipalities to DB has been finished");
     }
 }
